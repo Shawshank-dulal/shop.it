760e4a5df1ef7a9b51ef96463f6e857dbcb8ec41
Shawshank-dulal <sashank.dulal@gmail.com>
2021-04-25 18:54:41 +0545

    add cart

--- app/src/main/java/com/example/shopit/login.java
+++ app/src/main/java/com/example/shopit/login.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class login extends AppCompatActivity {
+public class login extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
-    public void switchToHomepage(View view) {
-        Intent homepage=new Intent(login.this, com.example.shopit.homepage.class);
-        startActivity(homepage);
+    public void switchToHomepage(View view){
+        Intent homepage=new Intent(login.this, com.example.shopit.homepage.class)
+        startActivity(homepage)
     }
+
+
 }
