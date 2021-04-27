306086edc01d507716c7c6e887ba0b51975992ba
Shawshank-dulal <sashank.dulal@gmail.com>
2021-04-27 20:12:53 +0545

    add scroll to homepage

--- app/src/main/java/com/example/shopit/MenuListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopit;
-
-import android.view.MenuItem;
-
-public interface MenuListener {
-    public abstract void onNavigationItemSelected (MenuItem item);
-}
--- app/src/main/java/com/example/shopit/homepage.java
+++ app/src/main/java/com/example/shopit/homepage.java
@@ -10,28 +10,11 @@ import android.widget.Toast;
 import static com.example.shopit.R.*;
 import static com.example.shopit.R.id.bottom_navigation;
 
-public class homepage extends AppCompatActivity implements MenuListener {
+public class homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_homepage);
     }
-
-
-    @Override
-    public void onNavigationItemSelected(MenuItem item) {
-        Toast toast = Toast.makeText(getApplicationContext(), "item.getItemId()", Toast.LENGTH_SHORT);
-        toast.show();
-
-//        switch (item.getItemId()){
-//            case getString(home):
-//                Intent homepage=new Intent(com.example.shopit.homepage.this, com.example.shopit.homepage.class);
-//
-//
-//            default:
-//                throw new IllegalStateException("Unexpected value: " + item);
-//        }
-//    }
-}
 }
