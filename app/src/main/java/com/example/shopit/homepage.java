308493faa1047d22ef8dc7eab78bac1d9e5e988f
sujan <sujantimalsina079@gmail.com>
2021-04-28 10:56:55 +0545

    Sldies

--- app/src/main/java/com/example/shopit/homepage.java
+++ app/src/main/java/com/example/shopit/homepage.java
@@ -1,20 +1,61 @@
 package com.example.shopit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+
+
+import com.example.shopit.adapter.HomeSliderAdapter;
+import com.example.shopit.data.SliderCotent;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.shopit.R.*;
-import static com.example.shopit.R.id.bottom_navigation;
 
 public class homepage extends AppCompatActivity {
 
+    SliderView sliderView;
+    RecyclerView.LayoutManager layoutManager;
+    HomeSliderAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_homepage);
+
+        ArrayList<SliderCotent> sliderContents=new ArrayList<>();
+
+        for(int i=0;i<5;i++)
+            sliderContents.add(
+                    new SliderCotent(
+                    drawable.robot,
+                    "Robot",
+                    19.25f,
+                    4.0f
+            ));
+
+
+
+        adapter=new HomeSliderAdapter();
+
+        layoutManager=new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+        sliderView=findViewById(id.imageSlider);
+
+        sliderView.setSliderAdapter(adapter);
+
+
+        adapter.setSliderCotents(sliderContents);
+
+
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
