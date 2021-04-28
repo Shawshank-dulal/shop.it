bb68804224feba3849930d64e5df63e6038cda5f
sashank dulal <sashank.dulal@gmail.com>
2021-04-28 12:05:39 +0545

    Revert "Slide"

--- app/src/main/java/com/example/shopit/MainActivity.java
+++ app/src/main/java/com/example/shopit/MainActivity.java
@@ -2,34 +2,13 @@ package com.example.shopit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-import java.util.Timer;
-import java.util.TimerTask;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity  {
-
-    Timer timer;
-
-      @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer=new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-            Intent login=new Intent(MainActivity.this,login.class);
-            startActivity(login);
-            finish();
-            }
-        },2000);
     }
-
-
-
 }
-
